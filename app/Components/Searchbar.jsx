3b0cf6d2d0a1511dc36a5a3a8004c93085ff280b
alhassan73 <alhassanahmed7396@gmail.com>
2024-01-05 02:47:13 +0200

    sixth

--- app/Components/Searchbar.jsx
+++ app/Components/Searchbar.jsx
@@ -4,28 +4,26 @@ export default function Searchbar() {
   return (
     <div className="w-full md:w-3/4 lg:w-1/2   flex flex-col justify-center items-center gap-7 mt-10">
       <div className="text-[#4effba]">
-        <h1 className="text-6xl md:text-7xl  font-bold ">
-          MOVIERSE
-        </h1>
+        <h1 className="text-6xl md:text-7xl  font-bold ">MOVIERSE</h1>
         <p className="text-sm md:text-base">
-          Welcome to a universe of cinematic delights , Whether you're a fan of
-          thrilling blockbusters, heartwarming dramas, or binge-worthy TV
-          series, you're in for a treat 🎬
+          Welcome to a universe of cinematic delights , Whether you&apos;re a
+          fan of thrilling blockbusters, heartwarming dramas, or binge-worthy TV
+          series, you&apos;re in for a treat 🎬
         </p>
       </div>
-      <div className="flex w-full justify-between items-center  text-lg search-container focus-within:border-[#4effba] border bg-black p-5 rounded-lg shadow-sm relative bg-transparent">
+      <div className="flex w-full justify-between items-center search-container focus-within:border-[#4effba] border bg-black p-5 rounded-lg shadow-sm relative bg-transparent">
         <button className="border-0 outline-0">
           <i className="fa-solid fa-magnifying-glass search-icon hover:text-[#4effba] text-white"></i>
         </button>
         <input
           type="text"
-          placeholder=" "
-          className="search-input absolute right-0 inset-y-1 border-0 outline-0 left-[10%] bg-black w-[90%]  ps-8 text-[#4effba] bg-transparent"
+          placeholder="Ex : Oppenheimer."
+          className="search-input absolute right-0 inset-y-1 border-0 outline-0 left-[10%] bg-black w-[90%] placeholder:text-transparent placeholder:text-[15px]  ps-8  bg-transparent"
           id="searchInput"
         />
         <label
           htmlFor="searchInput"
-          className="absolute cursor-text text-[16px] top-[26%] left-[15%] search-label text-zinc-400"
+          className="absolute cursor-text  top-[26%] left-[15%] search-label text-zinc-400"
         >
           Search your favorite
         </label>
--- app/Components/Trending.jsx
+++ app/Components/Trending.jsx
@@ -6,20 +6,33 @@ export default function Trending({ data }) {
       <div className="flex justify-between flex-wrap gap-8 mt-12 mx-auto">
         {data?.map((item, index) => {
           return (
-            <div key={index} className="w-[23%] bg-[#1c463685] p-3 rounded-lg">
-              <div className="image-container h-[90%] overflow-hidden rounded-lg">
+            <div
+              key={index}
+              className="w-[23%] bg-[#1c463685] p-3 rounded-xl trend relative"
+            >
+              <div className="rate absolute top-4 left-4 z-20 bg-[#1c4636] p-1 rounded-xl">
+                <i className="fa-regular fa-star me-1 text-amber-300"></i>
+                {Math.round(item.vote_average * 10) / 10}
+              </div>
+              <div className="image-container h-[90%] overflow-hidden rounded-xl">
                 <Image
                   src={`${baseUrl}${item.poster_path}`}
                   width={300}
                   height={300}
-                  className="w-full h-full"
+                  className="w-full h-full trend-img"
+                  alt={
+                    item.title ||
+                    item.original_title ||
+                    item.name ||
+                    item.original_name
+                  }
                 />
               </div>
               <h2 className="text-center font-bold mt-3 ">
                 {item.title?.split(" ").slice(0, 4).join(" ") ||
                   item.original_title?.split(" ").slice(0, 4).join(" ") ||
                   item.name?.split(" ").slice(0, 4).join(" ") ||
-                  original_name?.split(" ").slice(0, 4).join(" ")}
+                  item.original_name?.split(" ").slice(0, 4).join(" ")}
               </h2>
             </div>
           );
